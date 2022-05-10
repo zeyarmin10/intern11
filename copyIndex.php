9ac2fdd8e9c7fce43613996cf1ed6aa06bd4eff5
Zeyarmin <bcmm.intern11@brycenmyanmar.com.mm>
2022-05-10 16:11:14 +0630

    Add new file

---
+++ copyIndex.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>It is the first page!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, dicta minima! Consectetur, exercitationem. Qui, accusamus excepturi. Suscipit, reprehenderit, libero quis, unde laudantium aut placeat ratione consequatur voluptates qui dicta! Laudantium odio sit quaerat voluptatum eum cum ipsam aperiam accusamus aut sapiente reiciendis molestias numquam et obcaecati ipsum omnis consequatur a fugit maxime nisi, corrupti veritatis delectus asperiores. Inventore cupiditate veniam ex dolorem numquam dolor laboriosam ullam hic est ipsum molestias officia molestiae ad, quidem, libero mollitia ipsam. Iure suscipit labore amet explicabo voluptatibus in minus recusandae, minima expedita praesentium officia pariatur, enim ut maxime. Libero quo fuga exercitationem, enim suscipit cum cupiditate corporis dolores nam, explicabo ad rem, sed eaque totam illo aliquam quas modi labore alias aliquid optio. Minus natus unde beatae, saepe aliquid pariatur eos animi sapiente. Veritatis cumque et hic, facere dolorem minima provident animi eius nobis nemo incidunt aperiam obcaecati perferendis vel, laudantium necessitatibus. Laboriosam, eveniet. Perferendis dicta, odio earum et, dolores natus expedita tempore odit incidunt distinctio a quae porro assumenda possimus molestias illum dolorem mollitia doloribus necessitatibus nihil molestiae maxime delectus aperiam. Asperiores quae quidem laudantium repellat expedita, ea reiciendis consequatur sed sunt esse temporibus dolorem, nesciunt illum labore, veniam omnis aspernatur aut? Fugit similique id dolor totam officiis rem ipsum commodi minus expedita, exercitationem veritatis. Itaque debitis fugit necessitatibus sequi ab odit cumque! Molestiae incidunt corrupti facilis maxime aliquid soluta debitis earum quasi ab, distinctio blanditiis animi ut expedita harum dolor eligendi! Dolorem reprehenderit minima in vero consequuntur sed error? Quae vero deleniti temporibus eius! Fugit, praesentium officia dicta libero quo iure corporis ipsam. Sit labore blanditiis reprehenderit enim dicta soluta odit neque! Expedita, id nam, labore ab corrupti quas quod aperiam debitis laboriosam, harum nesciunt perspiciatis molestiae? Harum sed labore excepturi facere beatae magni provident repudiandae voluptatum odio numquam repellendus, sit saepe.</p>
+
+    <?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+    ?>
+
+</body>
+</html> 
